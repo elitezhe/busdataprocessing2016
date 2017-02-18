d42770ffce344f229a71e8bae78090963219a913
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-02-18 14:40:52 +0800

    Add distance calculation by 2 locations' lon and lat.

--- preprocess/preprocess_util.py
+++ preprocess/preprocess_util.py
@@ -7,6 +7,9 @@
 
 import pandas as pd
 import time
+from math import radians, sin, cos, sqrt, atan2
+
+
 df_head_with_index = \
     ['iindex', 'lon', 'lat', 'gpstime', 'gpsvelocity', 'northangle', 'vehstatus', 'alarmcode', 'orilon', 'orilat',
     'altitude', 'mile', 'fuel', 'enginetime', 'enginespeed', 'col14', 'col15', 'liquidtemper', 'voltage',
@@ -15,11 +18,13 @@ df_head_with_index = \
     'influxtemper', 'dooropen', 'col34', 'col35', 'driverid', 'col37', 'systime', 'soc', 'current', 'volt',
     'col42', 'gears', 'col44', 'col45', 'col46', 'col47', 'col48', 'col49']
 
+
 def systime2bj(unix_time):
     bj_format = '%Y-%m-%d %H:%M:%S'
     time_array = time.localtime(unix_time/1000)  # 转换为秒级来处理
     dt = time.strftime(bj_format, time_array)
     return dt
+
 
 def gpstime2bj(gpstime):
     bj_format = '%Y-%m-%d %H:%M:%S'
@@ -32,9 +37,33 @@ def gpstime2bj(gpstime):
     return dt
 
 
+def distance4gps(lon1, lat1, lon2, lat2):
+    """
+    reference: http://www.movable-type.co.uk/scripts/latlong.html
+    :param lon1:
+    :param lat1:
+    :param lon2:
+    :param lat2:
+    :return:
+    """
+    R = 6371e3  # metres
+    phi1 = radians(lat1)
+    phi2 = radians(lat2)
+    delta_phi = radians(lat2 - lat1)
+    delta_lambda = radians(lon2 - lon1)
+    a = sin(delta_phi/2.0) * sin(delta_phi/2.0) + \
+        cos(phi1) * cos(phi2) * \
+        sin(delta_lambda/2.0) * sin(delta_lambda/2.0)
+    c = 2.0 * atan2(sqrt(a), sqrt(1.0-a))
+    d = R * c
+    return d
+
+
 if __name__ == '__main__':
     unixtime = 1474238421303
     print systime2bj(unixtime)
 
     gpstime = ' 20160901 / 000124'
     print gpstime2bj(gpstime)
+
+    print distance4gps(116.23, 39.54, 115.48, 38.85)  # 100.3 km
